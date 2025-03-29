e8c82e6537fdeb152112ec726ececaaf57c9ea67
DESKTOP-13C4MS2\USER <GilaK>
2025-03-29 23:59:21 +0300

    images

--- src/Components/dashboard/dashboard.component.ts
+++ src/Components/dashboard/dashboard.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { UsersListComponent } from '../users-list/users-list.component';
 import { UserActivityGraphComponent } from '../user-activity-graph/user-activity-graph.component';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,11 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [UsersListComponent, UserActivityGraphComponent,
-    
-    MatCardModule,
-    MatListModule,
-    MatIconModule],
+  imports: [ UserActivityGraphComponent,MatCardModule,MatListModule, MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
